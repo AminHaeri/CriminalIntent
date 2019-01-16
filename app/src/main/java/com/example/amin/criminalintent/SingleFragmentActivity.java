0c7362030bc58ddc2d76dc81e150f17094e23d86
Amin <aminhaeri87@gmail.com>
2019-01-16 08:13:16 +0330

    test git

--- app/src/main/java/com/example/amin/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/amin/criminalintent/SingleFragmentActivity.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
+/**
+ * All activities that host one fragment must extend this class
+ */
 public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     public abstract Fragment createFragment();
